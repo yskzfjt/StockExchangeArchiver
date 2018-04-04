c7b7c6b5c496a427603050085f8cf57f61991b1f
fujita.y <fujita.y@tabot.jp>
2018-04-04 16:10:29 +0900

    envを追加。

--- hello_world/main.py
+++ hello_world/main.py
@@ -16,23 +16,6 @@
 import logging
 
 from flask import Flask
-import requests
-from datetime import datetime, timedelta
-
-url = "https://www.google.com/finance/getprices"
-code = 7203
-lsat_date = datetime.now() #データの取得開始日
-interval = 60  #データの間隔(秒)。1日 = 86400秒
-market = "TYO"  #取引所のコード　TYO=東京証券取引所
-period =  "2d" #データを取得する期間
-
-params = {
-  'q': code,
-  'i': interval,
-  'x': market,
-  'p': period,
-  'ts': lsat_date.timestamp()
-}
 
 
 app = Flask(__name__)
@@ -41,9 +24,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    r = requests.get(url, params=params)
-    return r.text.replace('\n', '<br>')
-    #return 'Hello World!'
+    return 'Hello World!'
 
 
 @app.errorhandler(500)
