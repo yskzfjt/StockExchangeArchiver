993d7d686af4b10aae2d70e8070c1b7d22e4f8a6
fujita.y <fujita.y@tabot.jp>
2018-04-04 16:55:27 +0900

    requestsを追加。

--- hello_world/main.py
+++ hello_world/main.py
@@ -17,6 +17,23 @@ import logging
 
 from flask import Flask
 
+import requests
+from datetime import datetime, timedelta
+
+url = "https://www.google.com/finance/getprices"
+code = 7203
+lsat_date = datetime.now() #データの取得開始日
+interval = 60  #データの間隔(秒)。1日 = 86400秒
+market = "TYO"  #取引所のコード　TYO=東京証券取引所
+period =  "2d" #データを取得する期間
+
+params = {
+  'q': code,
+  'i': interval,
+  'x': market,
+  'p': period,
+  'ts': lsat_date.timestamp()
+}
 
 app = Flask(__name__)
 
@@ -24,8 +41,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World!'
-
+    r = requests.get(url, params=params)
+    return r.text.replace('\n', '<br>')
+    #return 'Hello World!'
 
 @app.errorhandler(500)
 def server_error(e):
